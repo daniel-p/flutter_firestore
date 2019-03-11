496d01ad647e9a4c5b94393edc7ca2538ec4bd7c
koykova <47568998+koykova@users.noreply.github.com>
2019-03-11 14:46:48 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:Flutters/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('App title, theme and correct buttons are displayed', (WidgetTester tester) async {
